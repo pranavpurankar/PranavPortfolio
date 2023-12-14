9f55f228d84cf801fefc496e57affa46b726506f
Pranav Purankar <pranav.purankar@gmail.com>
2023-12-15 00:26:50 +0530

    portfolio testing commit

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*====== MENU SHOW ======*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+} 
+
+/*====== MENU SHOW ======*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navClose.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== SHADOW HEADER ===============*/
+
+
+/*=============== EMAIL JS ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
