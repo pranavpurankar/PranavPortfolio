5ecc80277a809421cdd5a8dffeadaa22ef29a565
pranavpurankar <pranav.purankar@gmail.com>
2023-12-21 14:56:45 +0530

    update portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,20 +19,88 @@ if(navClose){
     })
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/*====== MENU SHOW ======*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click',() =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    //when we click on each nav__link, we remove the show-menu click
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
 
 /*=============== SHADOW HEADER ===============*/
-
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    //when the scroll is greater than 50 viewport height, add the scroll-header class to the navbar
+    this.scrollY >= 50 ? header.classList.add('shadow-header'):header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll',shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_z7oxlqu','template_eb1ukpi','#contact-form','09joq1aGpcOAViNro')
+    .then(()=>{
+        // Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(()=>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        //Clear input fields
+        contactForm.reset()
+    }, ()=>{
+        //Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit',sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    //When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll'): scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = documents.querySelectorAll('section[id]')
 
+const scrollActive = ()=>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const   sectionHeight = current.offsetHeight,
+                sectionTop = current.offsetTop - 58,
+                sectionId = current.getAttribute('id'),
+                sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll',scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
