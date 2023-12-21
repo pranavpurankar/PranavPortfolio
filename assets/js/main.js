524992c6de2e30d94b61223b8cc98971106ad384
pranavpurankar <pranav.purankar@gmail.com>
2023-12-22 01:28:34 +0530

    final preview

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,3 +134,14 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin:'top',
+    distance: '69px',
+    duration:1500,
+    delay:200,
+    reset:true //Animation repeat
+})
+
+sr.reveal(`.home__perfil, .about__img, .contact__mail`, {origin:'right'})
+sr.reveal(`.home__name, .home__info, .about__container, .section__title-1, .about__info, .contact__social, .contact__data`, {origin:'left'})
+sr.reveal(`.services__card, .projects__card`, {interval:90})
